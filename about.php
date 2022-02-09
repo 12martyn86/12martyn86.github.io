f4c27cdc4d234fac40219de7e848351ee37a0fef
12martyn86 <99311062+12martyn86@users.noreply.github.com>
2022-02-09 08:20:07 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <?php require_once "blocks/head.php" ?>
+    </head>
+        <body>
+            <?php require_once "blocks/header.php" ?>
+            <?php require_once "blocks/main.php" ?>
+            <?php require_once "blocks/footer.php" ?>
+            
+            <script src="Js/script.js" ></script>
+    </body>
+</html>
